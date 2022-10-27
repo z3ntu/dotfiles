75b2547d0df301ebcbe82012deff3e6fc4e5b32a
Luca Weiss <luca@z3ntu.xyz>
2022-10-27 09:34:25 +0200

    fish: add otso_monitor_default function

---
+++ fish/.config/fish/functions/otso_monitor_default.fish
@@ -0,0 +1,4 @@
+function otso_monitor_default
+    echo "Setting default monitor settings for otso..."
+    kscreen-doctor output.1.scale.2 output.2.scale.1 output.1.position.0,1080 output.2.position.0,0 output.1.primary
+end
