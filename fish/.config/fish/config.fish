980f75e4c6d6958fd646767964f361e52f2fc28c
Luca Weiss <luca@z3ntu.xyz>
2022-10-27 09:34:03 +0200

    fish: clean up, add SSH_AUTH_SOCK

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,9 +6,6 @@ set -gx DIFFPROG "nvim -d"
 # virsh should connect to the system session by default
 set -gx LIBVIRT_DEFAULT_URI "qemu:///system"
 
-# set the pacaur directory
-set -gx AURDEST ~/aur
-
 # set variables for 'asp'
 set -gx ASPROOT ~/.asp
 set -gx ASPCACHE ~/.cache/asp/cache
@@ -17,6 +14,12 @@ set -gx ASPCACHE ~/.cache/asp/cache
 set -gx DEBFULLNAME "Luca Weiss"
 set -gx DEBEMAIL "luca@z3ntu.xyz"
 
+# ssh socket
+set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+
+# set up path
+fish_add_path -gm ~/bin
+
 # setup "thefuck"
 if test -x /usr/bin/thefuck
     thefuck --alias | source
@@ -24,25 +27,9 @@ end
 
 # convenience function
 function gpg_decrypt_clipboard --description 'Decrypt text on the clipboard via gpg'
-    xsel --clipboard | gpg --decrypt - $argv
+    if test $XDG_SESSION_TYPE = wayland
+        wl-paste | gpg --decrypt - $argv
+    else
+        xsel --clipboard | gpg --decrypt - $argv
+    end
 end
-
-if [ (hostname) = "arch-tower" ]
-    set -gx CCACHE_DIR /mnt/data1/ccache
-end
-
-if test -d ~/bin
-    set -gx PATH ~/bin $PATH
-end
-
-#function monstercatinstinct
-#   /usr/bin/streamlink --player mpv "https://www.youtube.com/watch?v=a_RMxE6bIo8" 480p &
-#   disown
-#end
-
-# start X at login
-#if status --is-login
-#    if test -z "$DISPLAY" -a $XDG_VTNR -eq 1
-#        exec startx -- -keeptty
-#    end
-#end
