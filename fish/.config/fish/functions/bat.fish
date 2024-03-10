75a2fc1af2c2a76dd0a6708fb1e1c9557c6088b5
Luca Weiss <luca@z3ntu.xyz>
2024-03-10 12:16:51 +0100

    fish/neovim: Update qdbus command to qdbus6

--- fish/.config/fish/functions/bat.fish
+++ fish/.config/fish/functions/bat.fish
@@ -1,5 +1,5 @@
 function bat --wraps='bat'
-    set konsole_theme (qdbus $KONSOLE_DBUS_SERVICE $KONSOLE_DBUS_SESSION profile)
+    set konsole_theme (qdbus6 $KONSOLE_DBUS_SERVICE $KONSOLE_DBUS_SESSION profile)
     if test $konsole_theme = 'Light'; or test -f /tmp/konsole-light-theme
         set -x BAT_THEME "Monokai Extended Light"
     end
