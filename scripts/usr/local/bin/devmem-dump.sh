9220c8307dfa5103ef8d39ce8de4ea76510973f9
Luca Weiss <luca@z3ntu.xyz>
2024-05-03 14:19:48 +0200

    scripts: devmem-dump: Support toybox 'width' parameter

--- scripts/usr/local/bin/devmem-dump.sh
+++ scripts/usr/local/bin/devmem-dump.sh
@@ -15,10 +15,21 @@ readonly size
 
 readonly end="$((start_addr + size))"
 
+# toybox (Android) and busybox interpret width as bytes vs bits
+if devmem --version 2>&1 | grep -q toybox; then
+    devmem_width=1 # bytes
+elif devmem --version 2>&1 | grep -q BusyBox; then
+    devmem_width=8 # bits
+else
+    echo "Unrecognized devmem version: $(devmem --version 2>&1)"
+    exit 1
+fi
+readonly devmem_width
+
 addr="$start_addr"
 while [ "$addr" -lt "$end" ]; do
     printf "0x%X - " "$addr"
-    devmem "$addr" 8
+    devmem "$addr" "$devmem_width"
 
     addr="$((addr + 4))"
 done
