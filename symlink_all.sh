b99d7e82f762fa08ced30b7aa9fcec9c3fdaeaf4
Luca Weiss <luca@lucaweiss.eu>
2026-01-12 18:14:02 +0100

    Update symlink_all.sh for removed configs

--- symlink_all.sh
+++ symlink_all.sh
@@ -2,10 +2,8 @@
 
 set -ex
 
-stow aur-out-of-date
 stow fish
 #stow fonts
-#stow i3
 stow neovim
 stow oh-my-fish
 stow ranger
@@ -14,9 +12,4 @@ stow systemd_user
 stow tmux
 sudo stow -t / firefox/
 sudo stow -t / fish-conf.d/
-sudo stow -t / grub/
-sudo stow -t / networkmanager/
-sudo stow -t / screen/
 sudo stow -t / scripts/
-sudo stow -t / udev/
-sudo stow -t / xorg-conf/
